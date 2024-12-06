9a18401c6f7ee90f0d2c9579058468957e70a4f2
Tu nombre <Tu correo>
2024-12-06 07:50:27 -0500

    Fin de seccion 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// [import { HeroComponent } from './heroes/hero/hero.component';
+// import { ListComponent } from './heroes/list/list.component';]
+import { CounterModule } from './counter/components/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // HeroComponent,
+    // ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    // AppRoutingModule,
+    HeroesModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
